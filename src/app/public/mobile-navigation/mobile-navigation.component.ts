940430e99333adbb1062198b0ba47571c3600c87
Ramegowda R <mohan.ramegowda.r@accenture.com>
2019-03-07 18:28:58 +0530

    Created Mobile navigation

---
+++ src/app/public/mobile-navigation/mobile-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mobile-navigation',
+  templateUrl: './mobile-navigation.component.html',
+  styleUrls: ['./mobile-navigation.component.scss']
+})
+export class MobileNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
