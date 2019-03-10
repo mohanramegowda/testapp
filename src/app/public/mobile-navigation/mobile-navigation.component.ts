616264c61ea38181d14a750db1703e0448cf7110
mohanramegowda <mohanramegowda7@gmail.com>
2019-03-10 22:20:54 +0530

    changed keyframe animation to use angular animation

--- src/app/public/mobile-navigation/mobile-navigation.component.ts
+++ src/app/public/mobile-navigation/mobile-navigation.component.ts
@@ -1,22 +1,43 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, OnDestroy, Input } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 type PaneType = 'left' | 'right';
 
 @Component({
   selector: 'mobile-navigation',
   templateUrl: './mobile-navigation.component.html',
-  styleUrls: ['./mobile-navigation.component.scss']
+  styleUrls: ['./mobile-navigation.component.scss'],
+  animations: [
+    trigger(
+      'enterAnimation', [
+        transition(':enter', [
+          style({transform: 'translateX(-100%)'}),
+          animate('500ms', style({transform: 'translateX(0)'}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateX(0)'}),
+          animate('500ms', style({transform: 'translateX(-100%)'}))
+        ])
+      ]
+    )
+  ]
 })
-export class MobileNavigationComponent implements OnInit {
+export class MobileNavigationComponent implements OnInit, OnDestroy {
   constructor() { }
+  @Input() ShowingMobileNavigation:boolean = false;
 
   @Output() SelectedMenuItem = new EventEmitter<boolean>();
 
   ngOnInit() {
+    //this.ShowingMobileNavigation = true;
   }
 
   SelectMenuItem(){
     this.SelectedMenuItem.emit(true);
+  }
+
+  ngOnDestroy(){
+    this.ShowingMobileNavigation = false;
   }
 
 }
