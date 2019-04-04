97e693eae81cb4ff5f8be31dd38666dbac72586f
Mohan Ramegowda R <mohan.ramegowda.r@accenture.com>
2019-04-04 20:50:19 +0530

    Implementated Pagination UI design

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemsToolbarComponent } from './items-toolbar/items-toolbar.component';
 import { PaginateComponent } from './paginate/paginate.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   exports: [ItemsToolbarComponent, PaginateComponent],
   declarations: [ItemsToolbarComponent, PaginateComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NgxPaginationModule
   ]
 })
 export class SharedModule { }
