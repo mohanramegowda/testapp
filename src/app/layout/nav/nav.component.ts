dc71ad03f92fdd3ec9188b56d38489e95cea71e8
Mohan R <mohan.r2@mphasis.com>
2019-12-19 21:23:04 +0530

    sidenav Advancement

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, ViewChildren, ContentChild, AfterContentInit } from '@angular/core';
+import { MatDrawerContent, MatSidenavContainer, MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, AfterViewInit {
 
+  @ViewChild(MatSidenavContainer, {static: false}) sideNavContainer;
+  @ViewChild('thisIsContent', {static: false}) sideNav;
+
+  @ContentChild(MatSidenavContent,{static: false}) sideNavCon;
   navItems = [
     { link: '/home', title: 'Home' },
     { link: '/get-started', title: 'Get Started' },
@@ -20,4 +25,9 @@ export class NavComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.sideNavContainer._element.nativeElement.className = 'mat-drawer-container-ext';
+    const classes = this.sideNav.nativeElement.offsetParent.className.replace('mat-drawer-content', 'mat-drawer-content-ext');
+    this.sideNav.nativeElement.offsetParent.className = classes;
+  }
 }
