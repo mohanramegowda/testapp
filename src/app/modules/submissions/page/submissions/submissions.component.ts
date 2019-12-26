35d736cad2b583c25533d0be026f768e8632cb3a
mohanramegowda <mohanramegowda7@gmail.com>
2019-12-26 16:13:46 +0530

    Submissions UI

--- src/app/modules/submissions/page/submissions/submissions.component.ts
+++ src/app/modules/submissions/page/submissions/submissions.component.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubmissionsComponent implements OnInit {
 
+  submissionList = [{
+    title: 'Swarga Rani',
+    totalSubmission: 2
+  },
+  {
+    title: 'NPS',
+    totalSubmission: 10
+  },
+  {
+    title: 'Baldvin',
+    totalSubmission: 11
+  },
+  {
+    title: 'Gnana Ganga Vidhya Peetha',
+    totalSubmission: 100
+  }];
+
   constructor() { }
 
   ngOnInit() {
