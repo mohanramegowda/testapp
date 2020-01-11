a54e85416a6646f9386705885437b13cbe1f0f29
mohanramegowda <mohanramegowda7@gmail.com>
2020-01-12 02:48:35 +0530

    Bank Account Details

--- src/app/modules/template-console/components/application-fee/page/application-fee/application-fee.component.ts
+++ src/app/modules/template-console/components/application-fee/page/application-fee/application-fee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-application-fee',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationFeeComponent implements OnInit {
 
-  constructor() { }
+  serviceFee: number;
+  form: FormGroup;
+  showPaypal: boolean;
+  showPayoneer: boolean;
+
+  payPalForm: FormGroup;
+
+  constructor() {
+    this.form = new FormGroup(
+      {
+        applicationFee: new FormControl('', [Validators.required])
+      }
+    )
+
+    this.payPalForm = new FormGroup({
+      emailAddress: new FormControl('', [Validators.required]),
+      confirmEmailAddress: new FormControl('', [Validators.required]),
+    });
+  }
 
   ngOnInit() {
+    this.serviceFee = 100;
+  }
+
+  ConvertStringToFloat(value) {
+    return parseFloat(value);
+  }
+
+  onheClick() {
+    console.log(this.showPaypal);
   }
 
 }
